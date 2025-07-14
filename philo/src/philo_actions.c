92df73ef3ab6510e90ed946de1db48d829798e1a
Artabarean <artabarean@gmail.com>
2025-07-14 16:31:41 +0200

    comeback

--- philo/src/philo_actions.c
+++ philo/src/philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 11:31:35 by alex              #+#    #+#             */
-/*   Updated: 2025/07/02 11:01:55 by alex             ###   ########.fr       */
+/*   Updated: 2025/07/14 16:17:35 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,9 @@ int pickforks(t_philosopher *philo)
 {
     pthread_mutex_t *first_fork;
     pthread_mutex_t *second_fork;
+    long long       tm;
 
+    tm = get_current_time();
     if (philo->id % 2 == 0)
     {
         first_fork = philo->left_fork;
@@ -47,7 +49,7 @@ int pickforks(t_philosopher *philo)
     }
     pthread_mutex_lock(&philo->aux->printofmutex);
     if (!philo->aux->stop)
-        printf("%lld %d has taken a fork\n", get_current_time() - philo->aux->start_time, philo->id);
+        printf("%lld %d has taken a fork\n", tm - philo->aux->start_time, philo->id);
     pthread_mutex_unlock(&philo->aux->printofmutex);
     pthread_mutex_lock(second_fork);
     if (philo->aux->stop)
@@ -58,7 +60,7 @@ int pickforks(t_philosopher *philo)
     }
     pthread_mutex_lock(&philo->aux->printofmutex);
     if (!philo->aux->stop)
-        printf("%lld %d has taken a fork\n", get_current_time() - philo->aux->start_time, philo->id);
+        printf("%lld %d has taken a fork\n", tm - philo->aux->start_time, philo->id);
     pthread_mutex_unlock(&philo->aux->printofmutex);
     return (0);
     
@@ -77,10 +79,22 @@ void    eat(t_philosopher *philo)
 
 void    put_down_fork(t_philosopher *philo)
 {
+    long long   tm;
 
+    tm = get_current_time();
+    pthread_mutex_lock(&philo->aux->printofmutex);
+    if (!philo->aux->stop)
+        printf("%lld %d puts down both forks\n", tm - philo->aux->start_time, philo->id);
+    pthread_mutex_unlock(&philo->aux->printofmutex);
 }
 
 void    philo_sleeps(t_philosopher *philo)
 {
-    
+    long long   tm;
+
+    tm = get_current_time();
+    pthread_mutex_lock(&philo->aux->printofmutex);
+    if (!philo->aux->stop)
+        printf("%lld %d sleeps\n", tm - philo->aux->start_time, philo->id);
+    pthread_mutex_unlock(&philo->aux->printofmutex);
 }
--- philo/src/struct_init.c
+++ philo/src/struct_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/18 12:11:12 by alex              #+#    #+#             */
-/*   Updated: 2025/06/30 13:38:26 by atabarea         ###   ########.fr       */
+/*   Updated: 2025/07/14 16:21:20 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ t_aux *struct_init(t_aux *aux, char *argv[], int argc)
     aux->eattime = ft_atol(argv[2]);
     aux->dietime = ft_atol(argv[3]);
     aux->sleeptime = ft_atol(argv[4]);
+    aux->start_time = get_current_time();
     if (argc == 6)
         aux->mealnum = ft_atol(argv[5]);
     else
