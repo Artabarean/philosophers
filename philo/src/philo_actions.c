ab0a4c64af5f0d1d17dbf9baafd692fcd9e2c43a
Alex Robert Tabarean <atabarea@c1r14s5.42madrid.com>
2025-07-15 13:05:23 +0200

    trying

--- philo/src/philo_actions.c
+++ philo/src/philo_actions.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_actions.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 11:31:35 by alex              #+#    #+#             */
-/*   Updated: 2025/07/14 16:17:35 by alex             ###   ########.fr       */
+/*   Updated: 2025/07/15 13:04:41 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,49 +21,40 @@ void    think(t_philosopher *philo)
     if (!philo->aux->stop)
         printf("%lld %d is thinking\n", tm - philo->aux->start_time, philo->id);
     pthread_mutex_unlock(&philo->aux->printofmutex);
-    
 }
 
 int pickforks(t_philosopher *philo)
 {
-    pthread_mutex_t *first_fork;
-    pthread_mutex_t *second_fork;
+    pthread_mutex_t *fst_fk;
+    pthread_mutex_t *sd_fk;
     long long       tm;
 
     tm = get_current_time();
     if (philo->id % 2 == 0)
     {
-        first_fork = philo->left_fork;
-        second_fork = philo->right_fork;
+        fst_fk = philo->left_fork;
+        sd_fk = philo->right_fork;
     }
     else
     {
-        first_fork = philo->right_fork;
-        second_fork = philo->left_fork;
+        fst_fk = philo->right_fork;
+        sd_fk = philo->left_fork;
     }
-    pthread_mutex_lock(first_fork);
+    pthread_mutex_lock(fst_fk);
     if (philo->aux->stop)
-    {
-        pthread_mutex_lock(first_fork);
-        return (-1);
-    }
+        return (pthread_mutex_unlock(fst_fk), -1);
     pthread_mutex_lock(&philo->aux->printofmutex);
     if (!philo->aux->stop)
-        printf("%lld %d has taken a fork\n", tm - philo->aux->start_time, philo->id);
+        printf("%lld %d took a fork\n", tm - philo->aux->start_time, philo->id);
     pthread_mutex_unlock(&philo->aux->printofmutex);
-    pthread_mutex_lock(second_fork);
+    pthread_mutex_lock(sd_fk);
     if (philo->aux->stop)
-    {
-        pthread_mutex_unlock(first_fork);
-        pthread_mutex_unlock(second_fork);
-        return (-1);
-    }
+        return (pthread_mutex_unlock(fst_fk), pthread_mutex_unlock(sd_fk), -1);
     pthread_mutex_lock(&philo->aux->printofmutex);
     if (!philo->aux->stop)
-        printf("%lld %d has taken a fork\n", tm - philo->aux->start_time, philo->id);
+        printf("%lld %d took a fork\n", tm - philo->aux->start_time, philo->id);
     pthread_mutex_unlock(&philo->aux->printofmutex);
     return (0);
-    
 }
 
 void    eat(t_philosopher *philo)
